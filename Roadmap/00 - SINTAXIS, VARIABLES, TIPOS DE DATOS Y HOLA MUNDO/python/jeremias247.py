ec90e3bbba217d9ae5eb502747d1695bd9e3e6e1
jeremias247 <jeremiasn373@gmail.com>
2025-11-24 16:25:50 -0300

    #00 - Python

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/jeremias247.py
@@ -0,0 +1,24 @@
+
+# Esto es un comentario para el curso de programacion desde 0
+"""Este comentario es para visualizar otra forma de hacer un comentario, valga la redundancia"""
+"""
+Este es otro comentario en multilinea
+"""
+'''
+Este es otro comentario multilinea pero con comillas simples
+'''
+
+# variables y constantes 
+mi_variable = "mi primer variable"
+MI_VARIABLE = "mi primer constante por convencion, no es constante como tal pero es una forma de avisar que es una constante"
+
+#tipos de datos primitivos
+mi_string = "mi cadena de texto con comillas dobles"
+mi_otro_string = 'mi cadena de texto con comillas simples'
+mi_booleano = True
+mi_booleano = False
+mi_entero = 10
+mi_flotante = 10.5
+
+# mi primera impresion en pantalla
+print("¡Hola mundo, Python!")
